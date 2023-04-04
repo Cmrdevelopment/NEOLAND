287184d198d13083ab14e291aa4a39f17d1b6441
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-04 14:53:03 +0100

    iteración1

---
+++ js_basic_algorithms/iteracion1.js
@@ -0,0 +1,19 @@
+// Variables
+
+// Iteración #1.1 (1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.)
+
+const myFavoriteHero = "Hulk";
+
+// Iteración #1.2 1.2 (Crea una variable llamada x, asigna el valor 50 a ella.)
+
+const x = 50;
+
+//Iteración #1.3 (1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.)
+
+const h = 5;
+const y = 10;
+
+//Iteración #1.4 (1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.)
+
+const z = h + y
+console.log(z);
