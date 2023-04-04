897c98f6f0885c15b78d90a2940dde89da0b5586
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-04 15:06:23 +0100

    iteración2

---
+++ js_basic_algorithms/iteracion2.js
@@ -0,0 +1,13 @@
+// Varaables avanzadas
+
+// Iteracion #2: 
+
+// 1.2 Declara 3 variables con los nombres y valores siguientes 
+
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
+
+console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " y me gustan los lobos");
+
+
