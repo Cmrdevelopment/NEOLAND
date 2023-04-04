421c65681ff40ab24e4dd1af9cb66c6e1eab088a
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-04 15:25:08 +0100

    iteración3

---
+++ js_basic_algorithms/iteracion3.js
@@ -0,0 +1,29 @@
+// Iteración #3: Operadores
+
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+let x = 10
+let y = 5
+console.log(x * y)
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+let z = 2
+console.log(x / z)
+
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
+
+// x = x + 5
+x += 5
+let o = x
+console.log(o)
+
+//1.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
+
+let c = 10
+c *= 5
+let i = c
+console.log(i)
+
+
+
