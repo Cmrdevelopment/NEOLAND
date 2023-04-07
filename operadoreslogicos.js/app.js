f179df02ae3cb9132f05570ef5b8f7cdb533f35d
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-07 19:44:19 +0100

    operadoreslogicos

---
+++ operadoreslogicos.js/app.js
@@ -0,0 +1,21 @@
+// Mini-reto operadores lógicos
+
+// 1.- Crea una variable llamada planet y asígnale el valor "Tierra"
+let planet = "Tierra"
+console.log(planet)
+
+// 2.- Crea una variable llamada isInnerPlanet y asígnale el valor true (la Tierra es un planeta interno)
+let isInnerPlanet = "true"
+
+// 3.- Crea una variable llamada hasAtmosphere y asígnale el valor true (la Tierra tiene atmósfera)
+let hasAtmosphere = "true"
+
+// 4.- Utiliza un operador lógico && (AND) para combinar las variables isInnerPlanet y hasAtmosphere en una expresión lógica que evalúe a true. 
+// Almacena el resultado en una variable llamada isHabitable.
+
+console.log(isInnerPlanet && hasAtmosphere);
+let isHabitable = "true"
+
+// 5.- tiliza una declaración console.log para imprimir el valor de la variable isHabitable en la consola. Debería mostrarse true.
+
+console.log(isHabitable);
