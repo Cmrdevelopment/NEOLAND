9230d68353cf098f5d442b89e6b5896e55573769
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-07 20:21:13 +0100

    operacionesdeasignacion

---
+++ operacionesdeasignacion/app.js
@@ -0,0 +1,37 @@
+// Asignar un valor a una variable
+
+let x = 5;
+
+// Incrementar una variable en una unidad
+
+let y = 0;
+y++; // la forma tradicional es y = y + 1
+
+// Decrementar una variable en una unidad
+
+let z = 5;
+z-- // la forma tradicional es z = z - 1
+
+// Asignar el valor de una variable a otra
+
+let a = 5;
+let b = a;
+console.log(b)
+
+// Asignar el resultado de una operación a una variable
+
+let c = 5;
+
+c += 3; // c = c + 3
+
+// Asignar el resultado de una operación a una variable utilizando una "VARIABLE" como operando
+
+let d = 5;
+let e = 3;
+
+d += e; // d = d + e
+
+// Asignar el resultado de una operación a una variable utilizando una "EXPRESIÓN" como operando
+
+let f = 5
+f += 3 + 4; // f = f + 3 + 4
