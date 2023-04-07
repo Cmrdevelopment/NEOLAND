a0b7ab6d88f172896820c6d32bfdbf479f7e7f2c
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-07 01:10:06 +0100

    prueba10

---
+++ js_basic_algorithms/prueba10.js
@@ -0,0 +1,31 @@
+let movie
+movie = "Titanic"
+console.log("movie")
+
+let pelicula = "Titanic"
+console.log("pelicula")
+
+const frase = "Yo soy el \"gato\" con botas"
+console.log(frase)
+
+const goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
+console.log(goodStr)
+
+const badStr = 'Finn responds, "Let\'s go!"'
+console.log(badStr)
+
+// Lista con
+// Tu nombre, Tu edad, ¿Eres desarollador?, Tu fecha de nacimiento, Tu libro Favorito con propiedades (titulo, autor, fecha, url)
+
+const lista = ["Carlos", 52, false];
+const lista2 = [22, 10, 1970];
+const libro = {
+    nombre: "Desgranando La Esencia del Mercado",
+    autor: "Carlos Martín",
+    fecha: "15 Noviembre 2019",
+    url: "https://cmrbolsa.com/mi-libro/",
+}
+
+console.log(lista);
+console.log(lista2);
+console.log(libro)
