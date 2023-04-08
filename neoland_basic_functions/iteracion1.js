24b3f7426f25faeab72fc6240fadf76490866dc3
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-08 12:11:49 +0100

    iteracion1

--- neoland_basic_functions/iteracion1.js
+++ neoland_basic_functions/iteracion1.js
@@ -1,30 +1,21 @@
 // Iteración #1: Buscar el máximo: Completa la función que tomando dos números como argumento devuelva el más alto
 
-// function maximo(arr) {
-//     let min = Math.min(...arr)
-//     let max = Math.max(...arr)
-//     return [min, max]
-// }
+function sum(num1, num2) {
+    if (num1 < num2) {
+        return num2
+    } else {
+        return num1
+    }
+}
+console.log(sum(30, 2))
 
 
-// maximo([25, 10, 2, 30, 5])
 
-// https://www.youtube.com/watch?v=gJj80WM-Sss
-
-// function minMax(arr) {
-//     let min = arr[0]
-//     let max = arr[0]
-//     for (let i = 0; i < arr.length; i++) {
-//         if (min = arr[i]) {
-//             min = arr[i]
-//         }
-//         if (max < arr[i]) {
-//             max = arr[i]
-//         }
-//     }
-//     return [min, max]
-// }
-
-// minMax([20, 4, 5, 30, 2])
-
-// console.log(minMax)
+function suma(numero1, numero2) {
+    if (numero1 > numero2) {
+        return numero1
+    } else {
+        return numero2
+    }
+}
+console.log(suma(500, 200))
