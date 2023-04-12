9289b72f69725baf48d7f5e62c4bffa244b5e717
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-12 22:57:49 +0100

    actualizacion

--- EJERCICIOS_NOTION/js_basic_algorithms/iteracion6.js
+++ EJERCICIOS_NOTION/js_basic_algorithms/iteracion6.js
@@ -24,14 +24,14 @@ for (i = 0; i <= 9; i++) {
 }
 
 // 0/2=	0   ==> Da cero debe monstrarse
-// 1/2= 0.5 ==> Da < o => no debe monstrarse
-// 2/2= 1   ==> Da < o => no debe monstrarse
-// 3/2=	1.5 ==> Da < o => no debe monstrarse
-// 4/2= 2   ==> Da < o => no debe monstrarse
-// 5/2=	2.5 ==> Da < o => no debe monstrarse
-// 6/2=	3   ==> Da < o => no debe monstrarse
-// 7/2=	3.5 ==> Da < o => no debe monstrarse
-// 8/2=	4   ==> Da < o => no debe monstrarse
-// 9/2=	4.5 ==> Da < o => no debe monstrarse
+// 1/2= 0.5 ==> Da > o => no debe monstrarse
+// 2/2= 1   ==> Da > o => no debe monstrarse
+// 3/2=	1.5 ==> Da > o => no debe monstrarse
+// 4/2= 2   ==> Da > o => no debe monstrarse
+// 5/2=	2.5 ==> Da > o => no debe monstrarse
+// 6/2=	3   ==> Da > o => no debe monstrarse
+// 7/2=	3.5 ==> Da > o => no debe monstrarse
+// 8/2=	4   ==> Da > o => no debe monstrarse
+// 9/2=	4.5 ==> Da > o => no debe monstrarse
 
 // 1.3 Crea un bucle para conseguir dormir contando ovejas. Este bucle tiene que dar 10 vueltas, es decir, 10 console.log. Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle y cambia el mensaje en la décima vuelta a 'Dormido!'.
