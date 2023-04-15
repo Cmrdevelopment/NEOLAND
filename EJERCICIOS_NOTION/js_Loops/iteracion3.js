2bc0f3cb642df17c4c0c362230eba6f8685a8d83
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-15 15:17:50 +0100

    actualizacion

---
+++ EJERCICIOS_NOTION/js_Loops/iteracion3.js
@@ -0,0 +1,18 @@
+// **Iteración #3: Probando For...of**
+
+// Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores.
+
+// Puedes usar este array:
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+// for (loquemedelagana of nombre de la variable) {
+/////  console.log(loquemedelagana);    
+// } 
+// ==> lo que hace el for of es poner un nombre cualquiera, después el of y después el nombre de la variable a tener en cuenta
+// el cosole.log(nombre de la variable a tener en cuenta) te da la lista del array
+
+
+for (recorrer of placesToTravel) {
+    console.log(recorrer);
+}
