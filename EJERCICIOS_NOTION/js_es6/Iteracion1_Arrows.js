a9ed9bce0ef72b3666f497ac79564d24c0eed9c1
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-16 20:29:48 +0100

    iteracion1

--- EJERCICIOS_NOTION/js_es6/Iteracion1_Arrows.js
+++ EJERCICIOS_NOTION/js_es6/Iteracion1_Arrows.js
@@ -2,23 +2,19 @@
 
 // Crea una arrow function que tenga dos parametros a y b y que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre por consola la suma de los dos parametros.
 
+// con ayuda de compañeros
+
 // 1.1 Ejecuta esta función sin pasar ningún parametro
 
-
-
-
-
-
-
+const sinParam = () => 10 + 5;
+console.log(sinParam());
 
 // 1.2 Ejecuta esta función pasando un solo parametro
 
-
-
-
-
-
-
-
+const conUnParam = (a) => a + 5;
+console.log(conUnParam(10))
 
 // 1.3 Ejecuta esta función pasando dos parametros
+
+const conDosParam = (a, b) => a + b;
+console.log(conDosParam(10, 5))
