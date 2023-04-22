f9f92f27e4023c13af5b81ea394e21633b208fb4
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-04-22 01:20:14 +0100

    actualización

---
+++ 01.TeoriaNeoland/10.Viernes21.04.2023_anima_y_dayseeker.band.js
@@ -0,0 +1,12 @@
+//? Animaciones > https://www.notion.so/neoland-school/Animaciones-fffb7a83eb4e450f892f6d35eac83c9a
+// Interpolación: es lo que hacen ahora como en anime, por ejemplo. Con 3 puntos de referencia pedes hacer una ainimación ya que el resto se lo inventa. CSS lo hace así !!!
+//! @keyframes + nombre de la animación (elquetuquieras)
+// Va por % 
+// Se puede hacer con listas, font, en geeral todo
+
+
+// Subrayar es con un hover y un border bottón
+
+// En lo videos ponemos:
+//---------------------> <video mute autoplay loop // mute que no suene, autoplya que se mueva, loop para que cuando termine empiece de nuevo
+//---------------------> <source src=""
