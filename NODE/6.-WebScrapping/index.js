7171db30881bce99410bdfc2c964b5804332cdf5
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-05-14 00:05:32 +0100

    actualización

---
+++ NODE/6.-WebScrapping/index.js
@@ -0,0 +1,8 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+const scrapping = async () => {
+  const BASE_URL = "https://www.game.es/buscar/brv/o=7&cf=000a_aa0211:5:GIDS";
+};
+
+//! A partir del minuto 7:34
