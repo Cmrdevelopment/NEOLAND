53ab19f10fe84e0c2a89173ab6e04aeeee08d49b
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-05-26 14:33:08 +0100

    a2

---
+++ 01.TeoriaNeoland/29.Jueves25.05.2023_ReactJS.js
@@ -0,0 +1,67 @@
+// ReactJS
+// Es una libreria que le instalas extensiones
+// Virtual DOM, es la representación de ese DOM y es que virtual DOM es mucho más rápido
+
+// Puede hacer las cosas como tú quieras con el limite de buenas prácticas
+
+// ¿Que es mejor? Angular ó ReactJS
+// Depende pero Angular es de google y tira mucho pero ReacJS es más agil y rapida
+
+// declarativas significa que no sabemos lo que ahce detrás, el desarrollador no sabe lo que hace detrás
+// Nosotros le damos ordenes y es el virtual DOM quien se encarga de hacer las cosas
+// Imperativo es que nosotros hacemos las cosas y lo mandamos, ejemlplo con: document.query.selector ==> esto es imperativo.
+// Basado en componentes
+
+// Trabajamos con funciones y no con clases, si es con clases es codigo muy antiguo
+
+//? PUNTOS CLAVE ==> en el presentación
+
+//! ReactJS son asincronas
+// Solo hace efecto el codigo que pongas (lógica) sin tener puesto un return y por eso es asincrono
+
+// Funciona con funciones a traves de Hooks (Gancho) que son las herramientas (USE). Se basa en Hook, en ganchos.
+
+//? npm create vite@...
+//? Le ponemos el nombre
+//? javascrip-SWC es la alternativa de Babbel
+// npm i
+// para correr el proyecto de desarrollo es npm run dev
+// App borramos el estdo y el template
+// App.js lo convertimos en arrow
+// App.css se borran
+// Index
+//! Seguir el VIDEO de clase
+// para hacer un componente hay que poner rafce
+
+// En html no se toca nada y solo se pone los link
+
+// En App es deonde tenemos nuestra aplicación
+
+// En html podemos meter en llaves {} JS directamte
+// Los estados son chivatos y produce un aviso de que se vuelve a rendedizar
+
+//! Instalar las extensiones del principio de curso
+
+// Que es una PROPS?
+
+//! Para meter código de JS en el retunr hay que ahbrir y cerrar llaves
+
+// ReactJS es un estado inmutable ya que no modificaremos un objeteto
+
+// Comunicación bidereccional
+// Lo natural es cambiar el estado del padre al hijo pero tmabien se puede cambiar un estado de un hijo de un padre
+//! En ReactJS el return SOLO puede tener un padre
+// Un fragment en ReactJS es <> y no aparece en el html y no aparece ahí y no fastidia cuando haga el css
+//! El efecto principal de un cambio de estado es hacer un refress, es decir, un redendizado.
+// Que elemento controla el usuario: Los botones
+//--->> Prevee los errores ReactJS. El Strict Mode es un doble render y solo lo hace dos rendent en desarrollo
+
+// Layauts es nuestra estructura de la página web
+
+// ponerle un padre a un componente es grapear. Cuando estás grapeando es children
+// Cuando es un cambio de estado es un PROP
+
+// Los estados pueden tener un LAZY, es decir, es un modo de actualizarse de otro modo
+
+//? public es para desplegar
+//? asset es para desarrollo
---
+++ 01.TeoriaNeoland/30.Viernes26.05.2023ReactJS_EstadosComplejos.js
@@ -0,0 +1,41 @@
+//? Estados complejos
+// Complejo significa que son objetos o array
+
+// Evento onChange: ¿Que es? cada vez que cambie su valor, tecleando su teclado
+
+// Mapear objetos
+// Vamos a recorrer objetos para pintarlos
+
+// Cuando no hay elemento padre todo lo que sea JS se debe poner <> </> dentro del return
+
+//TODO El punto 4 del repositorio de clase es un mapeo y hay un ejemplo de pagina básica, hacerla toda y entenderla.
+
+//? React Router ReactJS => https://reactrouter.com/en/main
+
+// Punto 5 del repositorio
+// Instalar React Router DOM => npm i react-router-dom
+
+// En main
+// Importar de la libraria > Browser es la etiqueta padre
+// Importar de la libraria > Routes y Route
+// Un path con un eslaz "/"
+
+// EN App vamos a tener todas nuestras paginas
+
+// El Outlet es como el Main ya que es dinámico y es la vista principal y es porque en main.jsx le llamamos index
+
+// El has es tiene para css en header.css
+
+// TODO IMPORTANTE
+//! Hacemos clicables con un link para elemento visual (imagen)
+//! Navigete es para botones
+//! NAV es un navlink
+
+// un dato mokeao son datos falsos,
+
+// Un if, foreach, etc se hace fuera del return --->> encima
+// Map, Ternario dentro del return
+
+// Los servicios (sin asincronias) son los empoint de una api, por cada una de las apis tendremos un servicios
+
+//TODO EJERCICIOS HACER LOS NINI-RETOS y EJERCICIOS
